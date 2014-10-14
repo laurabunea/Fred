24b65b1d63ece2c0c90747e661099eb07471c1d6
laurabunea <laura.r.bunea@gmail.com>
2014-10-14 17:13:40 +1300

    updates to forbar

--- www/js/biglink.js
+++ www/js/biglink.js
@@ -1,47 +1,46 @@
+// JavaScript Document
+
 /*
  * jquery.bigLink.js
- *
+ * Author: James Player - Springload (www.springload.co.nz) 2011
+ * 
  */
 
 (function($) {
     $.fn.bigLink = function(options) {
-        console.log("hey");
         var settings = {
             target    : 'a',
             avoid     : 'a'
         };
-
+        
         return this.each(function(){
             if (options){
                 $.extend(settings, options);
             }
-
+            
             var stop = false;
-
+            
             $(this).addClass('bigLink');
-
+            
             $(this).find(settings.avoid).click(function(e){
                 stop = true;
-            });
-
+            });          
+            
             $(this).click(function(e){
-
-                var url = $(this).find(settings.target).attr('href');
-
+                        
+                var url = $(this).find(settings.target).attr('href');     
+                
                 if (typeof(url) != 'undefined' && !stop){
                     if (e.metaKey){
                         window.open(url);
                     } else {
-                        window.location = url;
+                        window.location = url; 
                     }
                 }
-
-                stop = false;
-
             });
-
-
-
+            
+            
+            
         });
     };
 })( jQuery );
